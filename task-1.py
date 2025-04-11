616d6161919fa536b75cfb529953a67e993aea32
Gaurav280900 <kop504741@gmail.com>
2025-04-11 21:24:48 +0530

    This is for assignment purpose only

---
+++ task-1.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second number : "))
+add = a + b
+sub = a - b
+div = a / b
+mul = a * b
+print("Addition : " , add)
+print("Subtraction : ", sub)
+print("Division : ", div)
+print("Multiplication : ", mul)
---
+++ task-2.py
@@ -0,0 +1,3 @@
+a = input("Enter the first name : ")
+b = input("Enter the last name : ")
+print(f"Hello , {a} {b} !! Welcome to python programming")
